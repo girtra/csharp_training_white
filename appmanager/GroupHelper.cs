b01f2150849cce8f2929ffda60e4c8861ae8734c
Oksana Smiian <girtra@gmail.com>
2021-01-20 20:51:46 +0200

    remove group white

---
+++ appmanager/GroupHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.TreeItems;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using System.Windows.Automation;
+using TestStack.White.InputDevices;
+using TestStack.White.WindowsAPI;
+
+namespace addressbook_tests_white
+{
+    public  class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public GroupHelper(ApplicationManager manager) : base(manager)
+        {
+
+        }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree =  dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            foreach (var item in root.Nodes)
+            {
+                list.Add(new GroupData()
+                {
+                    Name = item.Text
+                });
+            }
+            CloseGroupsDialogue(dialogue);
+            return list;
+        }
+        public GroupHelper Remove(int index)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            Tree tree = dialogue.Get<Tree>("uxAddressTreeView");
+            TreeNode root = tree.Nodes[0];
+            root.Nodes[0].Click();
+            dialogue.Get<Button>("uxDeleteAddressButton").Click();
+            dialogue.Get<Button>("uxOKAddressButton").Click();
+            CloseGroupsDialogue(dialogue);
+            return this;
+        }
+
+        public GroupHelper Add(GroupData newGroup)
+        {
+            Window dialogue = OpenGroupsDialogue();
+            dialogue.Get<Button>("uxNewAddressButton").Click();
+            TextBox textBox = (TextBox)dialogue.Get(SearchCriteria.ByControlType(ControlType.Edit));
+            textBox.Enter(newGroup.Name);
+            Keyboard.Instance.PressSpecialKey(KeyboardInput.SpecialKeys.RETURN);
+            CloseGroupsDialogue(dialogue);
+            return this;
+        }
+
+        private void CloseGroupsDialogue(Window dialogue)
+        {
+            dialogue.Get<Button>("uxCloseAddressButton").Click();
+        }
+
+        private Window OpenGroupsDialogue()
+        {
+            manager.MainWindow.Get<Button>("groupButton").Click();
+            return manager.MainWindow.ModalWindow(GROUPWINTITLE);
+        }
+        
+    }
+}
---
+++ tests/GroupCreationTests.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void GroupCreationTest()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData newGroup = new GroupData()
+            {
+                Name = "testGroupNameWhite"
+            };
+            app.Groups.Add(newGroup);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
